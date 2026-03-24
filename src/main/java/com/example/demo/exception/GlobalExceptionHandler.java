76e9cce13cf11300bf13158e2665e079cb9a8f33
Cuk1e <2573816332@qq.com>
2026-03-24 11:38:40 +0800

    接口功能完善

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exception;
+
+import com.example.demo.common.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result<?> handleException(Exception e) {
+        e.printStackTrace();
+        return Result.error(500, "服务器内部错误");
+    }
+}
